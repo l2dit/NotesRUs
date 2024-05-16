7c2e3c20ad7b4569d0ab417b414c5ffc08b07d7e
Piuro <95382758+yPiuro@users.noreply.github.com>
2024-05-16 12:38:06 +1200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ async fn main() -> std::result::Result<(), std::io::Error> {
         "Notes R Us API Documentation",
         std::env::var("CARGO_PACKAGE_VERSION").unwrap_or(String::from("N/A")),
     )
-    .server("http://localhost:8000")
+    .server("http://localhost:80")
     .server("https://testapi.nzdev.org");
     let ui_docs_swagger = api_service.swagger_ui();
     let app = Route::new()
@@ -52,7 +52,5 @@ async fn main() -> std::result::Result<(), std::io::Error> {
                 .index_file("index.html"),
         );
 
-    Server::new(TcpListener::bind("0.0.0.0:8000"))
-        .run(app)
-        .await
+    Server::new(TcpListener::bind("0.0.0.0:80")).run(app).await
 }
