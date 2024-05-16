b5b4d9f6bc83683570b250a7fca45b198dca33ec
Piuro <95382758+yPiuro@users.noreply.github.com>
2024-05-16 12:33:57 +1200

    Update backend main.rs
    * Open API Documentation
    * Swagger UI docs
    * Simple path nesting
    * Startup API

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,58 @@
 use poem::{self, endpoint::StaticFilesEndpoint, listener::TcpListener, Route, Server};
+use poem_openapi::{
+    param::{self, Path, Query},
+    payload::{self, PlainText},
+    OpenApi, OpenApiService,
+};
 use std::env::{self};
-// use poem_openapi::{self, OpenApi}
+
+struct Api;
+
+#[derive(poem_openapi::ApiResponse)]
+enum Redirect {
+    #[oai(status = 302)]
+    Response(#[oai(header = "Location")] String),
+}
+
+#[OpenApi]
+impl Api {
+    /// Index / Docs
+    #[oai(path = "/", method = "get", tag = ApiTags::API)]
+    async fn index(&self) -> Redirect {
+        Redirect::Response("/api/docs".to_string())
+    }
+}
+
+#[derive(poem_openapi::Tags)]
+enum ApiTags {
+    /// All public API endpoints.
+    API,
+}
 
 #[tokio::main]
-async fn main() {
-    let app = Route::new().nest(
-        "/",
-        StaticFilesEndpoint::new(env::current_dir().unwrap().join("notes_r_us_ui/dist"))
-            .index_file("index.html"),
-    );
+async fn main() -> std::result::Result<(), std::io::Error> {
+    let api_service = OpenApiService::new(
+        Api,
+        "Notes R Us API Documentation",
+        std::env::var("CARGO_PACKAGE_VERSION").unwrap_or(String::from("N/A")),
+    )
+    .server("http://localhost:8000")
+    .server("https://testapi.nzdev.org");
+    let ui_docs_swagger = api_service.swagger_ui();
+    let app = Route::new()
+        .nest(
+            "/api",
+            Route::new()
+                .nest("/", api_service)
+                .nest("/docs", ui_docs_swagger),
+        )
+        .nest(
+            "/",
+            StaticFilesEndpoint::new(env::current_dir().unwrap().join("notes_r_us_ui/dist"))
+                .index_file("index.html"),
+        );
 
     Server::new(TcpListener::bind("0.0.0.0:8000"))
         .run(app)
         .await
-        .unwrap()
 }
