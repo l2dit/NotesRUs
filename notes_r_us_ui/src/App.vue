834b097d9e89a88cd6552512b4e61de612c9a96f
Ellozac <zlsy108@gmail.com>
2024-05-27 17:23:19 +1200

    added files :)

---
+++ notes_r_us_ui/src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div id="app">
+      <FileUpload />
+    </div>
+  </template>
+  
+  <script>
+  import FileUpload from './components/FileUpload.vue';
+  
+  export default {
+    components: {
+      FileUpload
+    }
+  };
+  </script>
+  
+  <style>
+  /* put syyles here 0_0*/
+  </style>
+  
---
+++ notes_r_us_ui/src/components/FileUpload.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form @submit.prevent="handle_submit">
+      <label>Upload markdown file.</label>
+      <br />
+      <input type="file" @change="handle_file_change" accept=".md, .markdown" />
+    </form>
+    <p v-if="file_content">{{ file_content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      file_content: ''
+    };
+  },
+  methods: {
+    handle_file_change(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const file_reader = new FileReader();
+        file_reader.onload = (e) => {
+          this.file_content = e.target.result;
+        };
+        file_reader.readAsText(file);
+      }
+    },
+    handle_submit() {
+      // put other form submit stuff here
+      console.log('form submitted succesfully');
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* styless stuff */
+</style>
