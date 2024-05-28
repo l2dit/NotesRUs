e9559dbafceef604ba64cc464a531efda0b6fea0
Ellozac <zlsy108@gmail.com>
2024-05-28 18:43:44 +1200

    Upload button workign routing at https://notesrus.nzdev.org/api/files/upload

--- notes_r_us_ui/src/components/FileUpload.vue
+++ notes_r_us_ui/src/components/FileUpload.vue
@@ -1,49 +1,50 @@
 <template>
   <div>
     <form @submit.prevent="handle_submit">
-      <label>Upload markdown file.</label>
+      <label>Upload markdown.</label>
       <br />
-      <input type="file" @change="handle_file_change" accept=".md, .markdown" />
+      <label id="file-input-button"for="file-input" >Upload</label>
+      <input id="file-input"type="file" @change="handle_file_change" accept=".md, .markdown"/>
     </form>
-    <p v-if="file_content">{{ file_content }}</p>
+    <p>{{ success }}</p>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-/* let post_route = window.location.hostname.concat("/file/upload"); */
-let post_route = "localhost:3000/api/file/upload"
+import axios from 'axios';
+
+let post_route = "https://notesrus.nzdev.org/api/file/upload";
+
 export default {
   data() {
     return {
-      file_content: ''
+      success: ''
     };
   },
   methods: {
     handle_file_change(event) {
       const file = event.target.files[0];
-      const fileForm = new FormData();
-      fileForm.append("file", file)
       if (file) {
+        const fileForm = new FormData();
+        fileForm.append("file", file);
 
-      axios.post(post_route, {
-        file: fileForm
-      }, {
-        headers: {
-          'Content-Type': 'text/md'
-        }
-      })
-
-        const file_reader = new FileReader();
-        file_reader.onload = (e) => {
-          this.file_content = e.target.result;
-        };
-        file_reader.readAsText(file);
+        axios.post(post_route, fileForm, {
+          headers: {
+            'Content-Type': 'text/markdown'
+          }
+        })
+        .then(response => {
+          this.success = "File uploaded successfully";
+          console.log('File uploaded successfully:', response.data);
+        })
+        .catch(error => {
+          this.success = `File failed to upload ${error}`;
+          console.error('Error uploading file:', error);
+        });
       }
     },
     handle_submit() {
-      // put other form submit stuff here
-      console.log('form submitted succesfully');
+      console.log('Form submitted successfully');
     }
   }
 };
@@ -54,4 +55,23 @@ export default {
   font-family: "Poppins", sans-serif;
   padding-left: 5%;
 }
+div form input[type=file] {
+  display: none;
+}
+#file-input-button {
+  transition: all 0.1s;
+  border: 1px solid #ccc;
+  display: inline-block;
+  padding: 6px 12px;
+  cursor: pointer;
+}
+
+#file-input-button:hover {
+  transition: all 0.3s;
+  border: 1px solid #000000;
+  display: inline-block;
+  padding: 6px 12px;
+  cursor: pointer;
+}
 </style>
+
