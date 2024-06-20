5aeaf3c5734ae9aab18851d5f6a130966b9f3fb7
Liam Tietjens <l.tietjens@hotmail.com>
2024-06-20 14:48:04 +1200

    adding the migration schema for users table

---
+++ migration/src/m20220101_000001_users_table.rs
@@ -0,0 +1,55 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Users::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Users::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Users::UserName).string().not_null())
+                    .col(ColumnDef::new(Users::FirstName).string().not_null())
+                    .col(ColumnDef::new(Users::LastName).string().not_null())
+                    .col(ColumnDef::new(Users::Role).string())
+                    .col(
+                        ColumnDef::new(Users::CreatedAt)
+                            .timestamp()
+                            .default(Expr::current_timestamp())
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .drop_table(Table::drop().table(Users::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Users {
+    Table,
+    Id,
+    UserName,
+    FirstName,
+    LastName,
+    Role,
+    CreatedAt,
+}
